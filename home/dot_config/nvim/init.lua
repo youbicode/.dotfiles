6999f4cb4b39780473d0c1d0816309550a424c1e
AKRAM EL YOUBI <elyoubi.akram@gmail.com>
2024-04-25 20:34:39 +0200

    feat: update nvim config

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,18 +1,2 @@
--- Set <space> as leader key before loading plugins
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {'git', 'clone', '--filter=blob:none',
-                 'https://github.com/folke/lazy.nvim.git',
-                 '--branch=stable', -- latest stable release
-  lazypath}
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Install plugins
-require('lazy').setup("plugins")
+require("youbiak.core")
+require("youbiak.lazy")
